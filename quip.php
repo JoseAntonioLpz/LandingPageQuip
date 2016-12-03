2ff07963bc464a72779aad7fcb3907dda8c81e29
JoseAntonioLpz <joseantoniolopez241197@gmail.com>
2016-12-03 17:34:02 +0100

    ...

--- quip.php
+++ quip.php
@@ -7,34 +7,25 @@
         Quip es una aplicación para teléfonos Android con la que podrás anotar todo lo que quieras en cualquier momento.
         <br>
         <h3 class="tipogra">Características</h3>
-        Con Quip puedes crear notas y listas y puedes añadir los siguientes elementos:
+            Con Quip puedes crear notas y listas y puedes añadir los siguientes elementos:
+            Quip ofrece la posibilidad de guardar fotografías, recien sacadas en tus notas ¡para que no tengas que escribir!, 
+        <br> además podrás guardar dibujos, ¡Quip dispone de una herramienta para hacerlos!, si quieres insertar una foto desde
+             tu galería, no te preocupes, ¡puedes hacerlo con Quip!.
+        <br> Quip tambien permite guardar notas de voz, ¡con tan solo darle a un botón!
+        <br> Tambien puedes escribir tus tareas en forma de listas, para saber que tienes que hacer en todo momento.
+        <br> Quip dispone de un sistema para añadir recordatorios a tus notas, ¡Para que no te olvides de ellas!
         <br>
-        &nbsp&nbsp -Imágenes.
-        <br>
-        &nbsp&nbsp -Notas de audio.
-        <br>
-        &nbsp&nbsp -Dibujos.
-        <br>
-        &nbsp&nbsp -Fotografías.
-        <br>
-        &nbsp&nbsp -Tareas.
-        <br>
-        &nbsp&nbsp -Recordatorios.
-        <br>
-        &nbsp&nbsp -PDFs.
-        <!--<h3>No pierdas tus notas</h3>
-        Con Quip puedes guardar tus notas en PDF para que no las pierdas nunca y puedas verlas en cualquier dispositivo.-->
+        <h3 class="tipogra">No pierdas tus notas</h3>
+        Con Quip puedes guardar tus notas en PDF para que no las pierdas nunca y puedas verlas en cualquier dispositivo.
     </div>
     <div class="descargar">
         <a href="https://play.google.com/store/apps/details?id=com.google.android.keep">
-            <img src="images/googleplay.png"
+            <img src="images/googleplay1.png"
                  onmouseout="this.style.opacity=1;this.filters.alpha.opacity='100';" 
                  onmouseover="this.style.opacity=0.6;this.filters.alpha.opacity='5';"/>
         </a>
-    </div>
-    <div class="descargarApple">
         <a href="https://play.google.com/store/apps/details?id=com.google.android.keep">
-            <img src="images/appstore.png"
+            <img src="images/appstore2.png"
                  onmouseout="this.style.opacity=1;this.filters.alpha.opacity='100';" 
                  onmouseover="this.style.opacity=0.6;this.filters.alpha.opacity='5';"/>
         </a>
